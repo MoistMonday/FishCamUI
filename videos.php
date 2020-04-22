208d840276c3495fc746dba843ba2a48268416b2
Elina <ebirgi17@student.aau.dk>
2020-04-22 11:34:44 +0200

    MiniProject

---
+++ videos.php
@@ -0,0 +1,57 @@
+<?php
+
+$link = mysqli_connect("localhost","root","");
+mysqli_select_db($link, "video");
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <link rel = "stylesheet" href = "style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600" rel="stylesheet">
+<meta charset = "utf-8">
+<title> FishCam: The Underwater World of Limfjorden </title>
+
+
+</head>
+<body class = "body">
+
+<div class = "feed">
+
+
+<?php
+
+$sql = "SELECT * FROM `uploaded_videos`";
+$result = $link -> query($sql) or die($link->error);
+
+while($row = $result->fetch_assoc())// mysqli_fetch_assoc($query))
+{
+    $id = $row['id'];
+    $name = $row['name'];
+    $url = $row['url'];
+    $time = $row ['time'];
+
+    //echo "<a href = 'watch.php?id=$id'> $name </a><br />";
+    echo '<div class = "videoBox">';
+    echo "<h2 class = 'videoTitle'> $time </h2>";
+    echo "<video class = 'video' width = '320' height = '240' controls src = '$url'> Your browser does not support the video tag. </video>";
+    echo "</div>";
+}
+
+
+?>
+<div class="videoBox header">
+<a href = "videos.php"> <img src= "fishlogo.png" height="25" width="25">  </a>
+</div>
+
+</div>
+
+<div class="videoBox btn-group">
+<a href = "upload.php"> <img src= "fishlogo.png" height="25" width="25">  </a>
+<a href = "videos.php"> <img src= "filledfishcam.png" height="25" width="25">  </a>
+<a href = "notification.php"> <img src= "bell.png" onmouseover = "this.src = 'filledbell.png'" onmouseout = "this.src = 'bell.png'"   height="25" width="25"> </a>
+<a href = "liked.php"> <img src= "heart.png" onmouseover = "this.src = 'filledheart.png'" onmouseout = "this.src = 'heart.png'" height="25" width="25"> </a>
+</div>
+</body>
+</html>
